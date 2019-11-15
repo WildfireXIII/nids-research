7594aa717b8c7b15665521d14b0c1eb9ea153c85
Nathan Martindale <nathanamartindale@gmail.com>
2019-11-15 17:12:12 -0600

    Fix data preprocess

--- R/kdd_data_preprocess.r
+++ R/kdd_data_preprocess.r
@@ -1,3 +1,5 @@
+library("foreign")
+
 df <- read.csv("../data/raw/kddcup/kddcup.data", head=F)
 
 # check types
@@ -7,6 +9,8 @@ str(df)
 # turn symbolic cols into symbolic
 # df$V2 <- as.factor(df$V2)
 # (not needed)
+
+
 
 # back,buffer_overflow,ftp_write,guess_passwd,imap,ipsweep,land,loadmodule,multihop,neptune,nmap,normal,perl,phf,pod,portsweep,rootkit,satan,smurf,spy,teardrop,warezclient,warezmaster.
 # duration: continuous.
@@ -50,3 +54,15 @@ str(df)
 # dst_host_srv_serror_rate: continuous.
 # dst_host_rerror_rate: continuous.
 # dst_host_srv_rerror_rate: continuous.
+
+col_names <- c("duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes", "land", "wrong_fragment", "urgent",
+               "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root",
+               "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login",
+               "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate",
+               "dst_host_count", "dst_host_srv_count", "dst_host_same_srv_rate", "dst_host_diff_srv_rate",
+               "dst_host_same_src_port_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate",
+               "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate", "target")
+
+names(df) <- col_names
+
+write.arff(df, "../data/raw/kddcup/kddcup.arff")
